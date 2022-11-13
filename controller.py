dab7e4d350e8213ce2b51dce5b25f7ec9bd5e07a
Santiago Valdez Ulzurrun <santiagoval1999@gmail.com>
2022-11-12 21:00:44 -0300

    fix: various errors with ifilters
    Co-Authored-By: Bruno Grassano <53836177+brunograssano@users.noreply.github.com>

--- controller.py
+++ controller.py
@@ -8,10 +8,12 @@ import os
 
 # Add your imports here ...
 from pox.lib.addresses import IPAddr
+import pox.lib.packet as pkt
 
 log = core.getLogger()
 
 # Add your global variables here ...
+IP = 0x0800
 
 class Firewall(EventMixin) :
     def __init__ ( self ) :
@@ -19,17 +21,70 @@ class Firewall(EventMixin) :
         log.debug("Enabling Firewall Module")
     
     def _handle_ConnectionUp(self, event):
-        # Add your logic here ...
-        my_match = of.ofp_match()
-        my_match.nw_dst = IPAddr("10.00.00.01")
-        my_match.tp_dst = 80
+        self.block_port(event, 80)
+        self.block_by(event, "10.0.0.1", 5001, "UDP")
+        self.block_communication_between(event, "10.0.0.2", "10.0.0.3")
+        log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
+    def block_port(self, event, port):
+        ''' Blocks all incoming packets to `port` number '''
+
+        # Mathes incoming packets that has port as port destination
+        portMatch = of.ofp_match()
+        portMatch.dl_type = IP
+        portMatch.nw_proto = pkt.ipv4.TCP_PROTOCOL
+        portMatch.tp_dst = port
+
+        # Changes routing table so that those packets are dropped
         msg = of.ofp_flow_mod()
-        msg.match = my_match
-
+        msg.match = portMatch
         event.connection.send(msg)
 
-        log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
+        portMatch = of.ofp_match()
+        portMatch.dl_type = IP
+        portMatch.nw_proto = pkt.ipv4.UDP_PROTOCOL
+        portMatch.tp_dst = port
+
+        msg = of.ofp_flow_mod()
+        msg.match = portMatch
+        event.connection.send(msg)
+
+        log.debug("Firewall block port %d rule installed on %s", port, dpidToStr(event.dpid))
+
+    def block_by(self, event, host, to_port, with_protocol):
+        ''' Blocks all incoming packets from `host` to `port` number `with_protocol` '''
+
+        hostToPortWithProtocolMatch = of.ofp_match()
+        hostToPortWithProtocolMatch.dl_type = IP
+        hostToPortWithProtocolMatch.nw_src = IPAddr(host)
+        hostToPortWithProtocolMatch.tp_dst = to_port
+        hostToPortWithProtocolMatch.nw_proto = pkt.ipv4.TCP_PROTOCOL if with_protocol == "TCP" else pkt.IPV4.UDP_PROTOCOL
+
+        msg = of.ofp_flow_mod()
+        msg.match = hostToPortWithProtocolMatch
+        event.connection.send(msg)
+
+        log.debug("Firewall block host %s to port %d with protocol %s rule installed on %s", host, to_port, with_protocol, dpidToStr(event.dpid))
+
+    def block_communication_between(self, event, host1, host2):
+        ''' Blocks all communication packets between `host1` and `host2` '''
+
+        host1ToHost2Match = of.ofp_match()
+        host1ToHost2Match.dl_type = IP
+        host1ToHost2Match.nw_src = IPAddr(host1)
+        host1ToHost2Match.nw_dst = IPAddr(host2)
+
+        msg = of.ofp_flow_mod()
+        msg.match = host1ToHost2Match
+        event.connection.send(msg)
+
+        host2ToHost1Match = host1ToHost2Match.flip()
+
+        msg = of.ofp_flow_mod()
+        msg.match = host2ToHost1Match
+        event.connection.send(msg)
+
+        log.debug("Firewall block communication with %s and %s rule installed on %s", host1, host2, dpidToStr(event.dpid))
 
 
 def launch():
